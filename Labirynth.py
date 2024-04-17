2a5cfbd57eba3aa42a9db18257e4ed23ced4b45d
Akmetari <77680749+Akmetari@users.noreply.github.com>
2024-04-17 16:34:45 +0200

    Labirynth and individual classes start

---
+++ Labirynth.py
@@ -0,0 +1,41 @@
+
+WALL=1
+EMPTY=0
+DEF_X_SIZE=20
+DEF_Y_SIZE=20
+DEF_START=(0,1)
+DEF_END=(DEF_X_SIZE,DEF_Y_SIZE)
+
+
+class Labirynth(object):
+    def __init__(self, x=DEF_X_SIZE, y=DEF_Y_SIZE, startPt=DEF_START, endPt=DEF_END):
+        self.xSize=x
+        self.ySize=y
+        self.startPoint=startPt
+        self.endPoint=endPt
+
+        self.matrix= []
+
+        for i in range(x): #creating def empty matrix
+            row=[]
+            for j in range(y):
+                row.append(EMPTY)
+            self.matrix.append(row)
+
+
+    def fillLabirynth(self,walls):  # wals in form of tuples representing boxes of labirynth
+        for w in walls:
+            self.matrix[w[0]][w[1]]=WALL #fills matrix with wall symbol where assigned by walls list
+
+    def passes(self, path):
+        passes=True;
+        if path[0]==self.startPoint & path[-1]==self.endPoint:
+            lastStep=self.startPoint
+            for step in path:
+                passes&=(abs(step[0]-lastStep[0])==1 ^ abs(step[1]-lastStep[1])==1) #checks if next step goes by 1 box exacly in one of two axes
+                if not passes:
+                    break
+
+        return passes
+
+
